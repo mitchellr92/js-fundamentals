b65e2817ad18c0396d91584347890ff6b263b2d5
Mitchell Robles <mitchellr92@live.com>
2021-11-01 16:45:20 -0700

    get element by class name

---
+++ js fundamentals/dom selectors for multiple elements/app.js
@@ -0,0 +1,22 @@
+// // document.getElementsByClassName
+
+// const items = document.getElementsByClassName("collection-item");
+
+// console.log(items);
+// console.log(items[0]);
+// items[0].style.color = "red";
+// items[3].textContent = "Hello World";
+
+// const listItems = document
+//   .querySelector("ul")
+//   .getElementsByClassName("collection-item");
+
+// console.log(listItems);
+
+// document.getElementByTagName
+const lis = document.getElementsByTagName("li");
+
+console.log(lis);
+console.log(lis[0]);
+lis[0].style.color = "red";
+lis[3].textContent = "Hello World";
