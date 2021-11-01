613d55690c62c6c0408741eb5377f2dc19db1140
Mitchell Robles <mitchellr92@live.com>
2021-11-01 16:29:14 -0700

    get element by id

---
+++ js fundamentals/dom selectors/app.js
@@ -0,0 +1,19 @@
+// document.getElementById()
+
+console.log(document.getElementById("task-title"));
+
+// Get things from the element
+console.log(document.getElementById("task-title").id);
+console.log(document.getElementById("task-title").className);
+
+// Change styling
+document.getElementById("task-title").style.background = "#333";
+document.getElementById("task-title").style.color = "#fff";
+document.getElementById("task-title").style.passing = "5px";
+// document.getElementById("task-title").style.display = "none";
+
+// Change content
+document.getElementById("task-title").textContent = "Task List";
+document.getElementById("task-title").innerText = "My Tasks";
+document.getElementById("task-title").innerHTML =
+  "<span style='color: red'>Task List</span>";
