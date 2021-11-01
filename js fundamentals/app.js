77ac808f59f67972ab29207f64e1533c4de440ff
Mitchell Robles <mitchellr92@live.com>
2021-11-01 16:14:43 -0700

    accessing dom elements

--- js fundamentals/app.js
+++ js fundamentals/app.js
@@ -18,7 +18,24 @@ val = document.forms[0].id;
 val = document.forms[0].method;
 val = document.forms[0].action;
 
-val = document.links
+val = document.links;
+val = document.links[0];
+val = document.links[0].id;
+val = document.links[0].className;
+val = document.links[0].classList;
+val = document.links[0].classList[0];
 
+val = document.images;
+
+val = document.scripts;
+val = document.scripts[2].getAttribute("src");
+
+let scripts = document.scripts;
+
+let scriptsArray = Array.from(scripts);
+
+scriptsArray.forEach(function(script) {
+  console.log(script.getAttribute("src"));
+});
 
 console.log(val);
