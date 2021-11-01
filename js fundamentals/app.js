7b938142f396ed4a4891ce17a7648686bce8b2be
Mitchell Robles <mitchellr92@live.com>
2021-11-01 16:09:44 -0700

    accessing dom

--- js fundamentals/app.js
+++ js fundamentals/app.js
@@ -4,5 +4,21 @@ val = document;
 val = document.all;
 val = document.all[2];
 val = document.all.length;
+val = document.head;
+val = document.body;
+val = document.doctype;
+val = document.domain;
+val = document.URL;
+val = document.characterSet;
+val = document.contentType;
+
+val = document.forms;
+val = document.forms[0];
+val = document.forms[0].id;
+val = document.forms[0].method;
+val = document.forms[0].action;
+
+val = document.links
+
 
 console.log(val);
