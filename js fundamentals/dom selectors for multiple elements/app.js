fabf1ba85ac806b10b7cc57f800944d5fcc1ced0
Mitchell Robles <mitchellr92@live.com>
2021-11-01 16:53:55 -0700

    query selector all

--- js fundamentals/dom selectors for multiple elements/app.js
+++ js fundamentals/dom selectors for multiple elements/app.js
@@ -32,3 +32,24 @@
 // });
 
 // console.log(lis);
+
+// document.querySelectorAll()
+const items = document.querySelectorAll("ul.collection li.collection-item");
+
+items.forEach(function(item, index) {
+  console.log(item.className);
+  item.textContent = `${index}: hello`;
+});
+
+const liOdd = document.querySelectorAll("li:nth-child(odd)");
+const liEven = document.querySelectorAll("li:nth-child(even)");
+
+liOdd.forEach(function (li, index) {
+  li.style.background = "#ccc"
+});
+
+for (let i = 0; i < liEven.length; i++) {
+  liEven[i].style.background = "#f4f4f4"
+}
+
+console.log(items);
