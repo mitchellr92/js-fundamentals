46d0b5697057e9600ae8f4f883a7dbd39e847448
Mitchell Robles <mitchellr92@live.com>
2021-11-05 08:57:08 -0700

    clear tasks from local storage

--- DOM projects/task list/app.js
+++ DOM projects/task list/app.js
@@ -105,8 +105,29 @@ function removeTask(e) {
   if (e.target.parentElement.classList.contains("delete-item")) {
     if (confirm("Are you sure?")) {
       e.target.parentElement.parentElement.remove();
+
+      // Remove from local storage
+      removeTaskFromLocalStorage(e.target.parentElement.parentElement);
     }
   }
+}
+
+// Remove task from local storage
+function removeTaskFromLocalStorage(taskItem) {
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+
+  tasks.forEach(function(task, index) {
+    if (taskItem.textContent === task) {
+      tasks.splice(index, 1);
+    }
+  });
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
 // Clear Tasks
@@ -120,6 +141,14 @@ function clearTasks(e) {
   }
 
   // https://jsperf.com/innerhtml-vs-removechild
+
+  // Clear tasks from local storage
+  clearTasksFromLocalStorage();
+}
+
+// Clear tasks from local storage
+function clearTasksFromLocalStorage() {
+  localStorage.clear();
 }
 
 // Filter tasks
