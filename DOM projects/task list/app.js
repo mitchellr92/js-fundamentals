fd756cc97b74ecacd415b676e6208a55b9241178
Mitchell Robles <mitchellr92@live.com>
2021-11-05 08:15:26 -0700

    create li item and append to task list on submit

--- DOM projects/task list/app.js
+++ DOM projects/task list/app.js
@@ -17,8 +17,29 @@ function loadEventListeners() {
 // Add task
 function addTask(e) {
   e.preventDefault();
-  
+
   if (taskInput.value === "") {
     alert("Add a task");
   }
+
+  // Create li element
+  const li = document.createElement("li");
+  // Add class
+  li.className = "collection-item";
+  // Create text node and append to li
+  li.appendChild(document.createTextNode(taskInput.value));
+  // Create new link element
+  const link = document.createElement("a");
+  // Add class
+  link.className = "delete-item secondary-content";
+  // Add icon html
+  link.innerHTML = "<i class='fa fa-remove'></i>";
+  // Append link to li
+  li.appendChild(link);
+
+  // Append li to ul
+  taskList.appendChild(li);
+
+  // Clear input
+  taskInput.value = "";
 }
