8598e4d8924668613ed2208ce23fd44a3faf56c6
Mitchell Robles <mitchellr92@live.com>
2021-11-05 08:07:37 -0700

    define global variables and add some event listners

---
+++ DOM projects/task list/app.js
@@ -0,0 +1,24 @@
+// Define UI vars
+const form = document.querySelector("#task-form");
+const taskList = document.querySelector(".collection");
+const clearBtn = document.querySelector(".clear-tasks");
+const filter = document.querySelector("#filter");
+const taskInput = document.querySelector("#task");
+
+// Load all event listeners
+loadEventListeners();
+
+// Load all event listeners
+function loadEventListeners() {
+  // Add task event
+  form.addEventListener("submit", addTask);
+}
+
+// Add task
+function addTask(e) {
+  e.preventDefault();
+  
+  if (taskInput.value === "") {
+    alert("Add a task");
+  }
+}
