10944b1ba48572c028c3903f6011d7e59f506a57
Mitchell Robles <mitchellr92@live.com>
2021-11-08 09:38:28 -0800

    error alert if fields are not filled out

--- DOM projects/loan calculator/app.js
+++ DOM projects/loan calculator/app.js
@@ -11,7 +11,7 @@ function calculateResults(e) {
   const interest = document.getElementById("interest");
   const years = document.getElementById("years");
   const monthlyPayment = document.getElementById("monthly-payment");
-  const totalPayment = document.getElementById("totalPayment");
+  const totalPayment = document.getElementById("total-payment");
   const totalInterest = document.getElementById("total-interest");
 
   const principal = parseFloat(amount.value);
@@ -22,13 +22,38 @@ function calculateResults(e) {
   const x = Math.pow(1 + calculatedInterest, calculatedPayments);
   const monthly = (principal * x * calculatedInterest) / (x - 1);
 
-  if (idFinite(monthly)) {
+  if (isFinite(monthly)) {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
   } else {
-    
+    showError("Please check your numbers");
   }
+}
 
-  console.log(e);
+// Show error
+function showError(error) {
+  // Create div
+  const errorDiv = document.createElement("div");
+
+  // Get elements
+  const card = document.querySelector(".card");
+  const heading = document.querySelector(".heading");
+
+  // Add class
+  errorDiv.className = "alert alert-danger";
+
+  // Create text node and append to div
+  errorDiv.appendChild(document.createTextNode(error));
+
+  // Insert error above heading
+  card.insertBefore(errorDiv, heading);
+
+  // Clear error after 3 seconds
+  setTimeout(clearError, 3000);
+}
+
+// Clear error
+function clearError() {
+  document.querySelector(".alert").remove();
 }
