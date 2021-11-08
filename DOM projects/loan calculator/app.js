c3640a9bf9127541ac36adf2151566e50cac3551
Mitchell Robles <mitchellr92@live.com>
2021-11-08 09:46:22 -0800

    loading gif for user experience

--- DOM projects/loan calculator/app.js
+++ DOM projects/loan calculator/app.js
@@ -1,11 +1,17 @@
 // Listen for submit
-document
-  .getElementById("loan-form")
-  .addEventListener("submit", calculateResults);
+document.getElementById("loan-form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  // Hide results
+  document.getElementById("results").style.display = "none";
+
+  // Show loader
+  document.getElementById("loading").style.display = "block";
+
+  setTimeout(calculateResults, 1000);
+});
 
 // Calculate results
-function calculateResults(e) {
-  e.preventDefault();
+function calculateResults() {
   // UI variables
   const amount = document.getElementById("amount");
   const interest = document.getElementById("interest");
@@ -26,6 +32,12 @@ function calculateResults(e) {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
+
+    // Show results
+    document.getElementById("results").style.display = "block";
+
+    // Hide loader
+    document.getElementById("loading").style.display = "none";
   } else {
     showError("Please check your numbers");
   }
@@ -33,6 +45,13 @@ function calculateResults(e) {
 
 // Show error
 function showError(error) {
+  // Hide results
+  document.getElementById("results").style.display = "none";
+
+  // Hide loader
+  document.getElementById("loading").style.display = "none";
+
+
   // Create div
   const errorDiv = document.createElement("div");
 
