93d4f149b0f3138cabf68e1b04f1f2515753469d
Mitchell Robles <mitchellr92@live.com>
2022-01-19 10:41:23 -0800

    persist data with local storage

--- OOP Book List Project/appes6.js
+++ OOP Book List Project/appes6.js
@@ -55,6 +55,53 @@ class UI {
   }
 }
 
+// Local storage class
+class Store {
+  static getBooks() {
+    let books;
+    if (localStorage.getItem("books") === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem("books"));
+    }
+
+    return books;
+  }
+
+  static displayBooks() {
+    const books = Store.getBooks();
+    books.forEach(function(book) {
+      const ui = new UI();
+
+      // Add book to UI
+      ui.addBookToList(book);
+    });
+  }
+
+  static addBook(book) {
+    const books = Store.getBooks();
+    books.push(book);
+
+    localStorage.setItem("books", JSON.stringify(books));
+  }
+
+  static removeBook(isbn) {
+    const books = Store.getBooks();
+    books.forEach(function(book, index) {
+      if (book.isbn === isbn) {
+        books.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem("books", JSON.stringify(books));
+  }
+}
+
+//----------EVENT LISTENERS----------
+
+// DOM load event
+document.addEventListener("DOMContentLoaded", Store.displayBooks);
+
 // Event listener for add book
 document.getElementById("book-form").addEventListener("submit", function(e) {
   e.preventDefault();
@@ -77,6 +124,9 @@ document.getElementById("book-form").addEventListener("submit", function(e) {
     // Add book to list
     ui.addBookToList(book);
 
+    // Add to local storage
+    Store.addBook(book);
+
     // Show success
     ui.showAlert("Book Added!", "success");
 
@@ -93,6 +143,9 @@ document.getElementById("book-list").addEventListener("click", function(e) {
   const ui = new UI();
 
   ui.deleteBook(e.target);
+
+  // Remove from local storage
+  Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
 
   // Show message
   ui.showAlert("Book Removed Successfully", "success");
