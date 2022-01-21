0acc992ddb37c4a15a9d3d7e7b2df57cfb87557a
Mitchell Robles <mitchellr92@live.com>
2022-01-21 10:33:55 -0800

    Ajax with callbacks, post put and delete requests

--- Asynchronous JS/Easy HTTP/app.js
+++ Asynchronous JS/Easy HTTP/app.js
@@ -1,7 +1,61 @@
 const http = new easyHTTP();
 
 // Get Posts
-http.get("https://jsonplaceholder.typicode.com/posts", function(
+// http.get("https://jsonplaceholder.typicode.com/posts", function(
+//   error,
+//   response
+// ) {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(response);
+//   }
+// });
+
+// Get single post
+// http.get("https://jsonplaceholder.typicode.com/posts/1", function (
+//   error,
+//   response
+// ) {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(response);
+//   }
+// });
+
+// Create Data
+const data = {
+  title: "Custom Post",
+  body: "This is a custom post"
+};
+
+// Create post
+// http.post("https://jsonplaceholder.typicode.com/posts", data, function(
+//   error,
+//   response
+// ) {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(response);
+//   }
+// });
+
+// Update post
+// http.put("https://jsonplaceholder.typicode.com/posts/45", data, function(
+//   error,
+//   response
+// ) {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(response);
+//   }
+// });
+
+// Delete post
+http.delete("https://jsonplaceholder.typicode.com/posts/1", function(
   error,
   response
 ) {
