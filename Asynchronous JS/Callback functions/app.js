3da3790ad66c61e1b134ed6113fa6ec9a34cdfc0
Mitchell Robles <mitchellr92@live.com>
2022-01-21 10:55:11 -0800

    ES6 Promises

--- Asynchronous JS/Callback functions/app.js
+++ Asynchronous JS/Callback functions/app.js
@@ -8,6 +8,37 @@ const posts = [
     body: "This is post two"
   }
 ];
+function createPost(post) {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      posts.push(post);
+      const error = true;
+
+      if (!error) {
+        resolve();
+      } else {
+        reject("Error: Something went wrong");
+      }
+    }, 2000);
+  });
+}
+
+function getPosts() {
+  setTimeout(function() {
+    let output = "";
+    posts.forEach(function(post) {
+      output += `<li>${post.title}</li>`;
+    });
+
+    document.body.innerHTML = output;
+  }, 1000);
+}
+
+createPost({ title: "Post Three", body: "This is post three" })
+  .then(getPosts)
+  .catch(function(error) {
+    console.log(error);
+  });
 
 //
 //
@@ -44,22 +75,22 @@ const posts = [
 //
 //
 //
-function createPost(post, callback) {
-  setTimeout(function() {
-    posts.push(post);
-    callback();
-  }, 2000);
-}
+// function createPost(post, callback) {
+//   setTimeout(function() {
+//     posts.push(post);
+//     callback();
+//   }, 2000);
+// }
 
-function getPosts() {
-  setTimeout(function() {
-    let output = "";
-    posts.forEach(function(post) {
-      output += `<li>${post.title}</li>`;
-    });
+// function getPosts() {
+//   setTimeout(function() {
+//     let output = "";
+//     posts.forEach(function(post) {
+//       output += `<li>${post.title}</li>`;
+//     });
 
-    document.body.innerHTML = output;
-  }, 1000);
-}
+//     document.body.innerHTML = output;
+//   }, 1000);
+// }
 
-createPost({ title: "Post Three", body: "This is post three" }, getPosts);
+// createPost({ title: "Post Three", body: "This is post three" }, getPosts);
