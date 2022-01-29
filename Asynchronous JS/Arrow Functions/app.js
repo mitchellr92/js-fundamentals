fa7a93e8f681a7da2a06666b5c51f9c45b825f65
Mitchell Robles <mitchellr92@live.com>
2022-01-28 16:27:47 -0800

    converting to arrow functions

---
+++ Asynchronous JS/Arrow Functions/app.js
@@ -0,0 +1,5 @@
+// const sayHello = () => console.log('Hello')
+
+const sayHello = () => 'Hello';
+
+sayHello()
--- Asynchronous JS/Fetch API/app.js
+++ Asynchronous JS/Fetch API/app.js
@@ -4,23 +4,17 @@ document.getElementById("button3").addEventListener("click", getExternal);
 
 function getText() {
   fetch("test.txt")
-    .then(function(res) {
-      return res.text();
-    })
-    .then(function(data) {
+    .then(res => res.text())
+    .then(data => {
       document.getElementById("output").innerHTML = data;
     })
-    .catch(function(err) {
-      console.log(err);
-    });
+    .catch(err => console.log(err));
 }
 
 function getJson() {
   fetch("post.json")
-    .then(function(res) {
-      return res.json();
-    })
-    .then(function(data) {
+    .then(res => res.json())
+    .then(data => {
       console.log(data);
       let output = "";
       data.forEach(function(post) {
@@ -28,17 +22,13 @@ function getJson() {
         document.getElementById("output").innerHTML = output;
       });
     })
-    .catch(function(err) {
-      console.log(err);
-    });
+    .catch(err => console.log(err));
 }
 
 function getExternal() {
   fetch("https://api.github.com/users")
-    .then(function(res) {
-      return res.json();
-    })
-    .then(function(data) {
+    .then(res => res.json())
+    .then(data => {
       console.log(data);
       let output = "";
       data.forEach(function(user) {
@@ -46,7 +36,5 @@ function getExternal() {
         document.getElementById("output").innerHTML = output;
       });
     })
-    .catch(function(err) {
-      console.log(err);
-    });
+    .catch(err => console.log(err));
 }
